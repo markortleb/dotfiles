80eecfe4705cc0a2cc78ea4e5530749974bcf29a
Mark Ortleb <markortleb@gmail.com>
2025-04-26 01:38:18 -0400

    got lsp working with completion

--- nvim/lua/markortleb/plugins/lazy.lua
+++ nvim/lua/markortleb/plugins/lazy.lua
@@ -1,17 +1,22 @@
 require("lazy").setup({
   
-  { "neovim/nvim-lspconfig" },
+  -- Dadbod SQL Completer
+  { "tpope/vim-dadbod" },
+  { "kristijanhusak/vim-dadbod-ui" },
+  { "kristijanhusak/vim-dadbod-completion" }, 
+  
+  -- LSP and Mason
+  { import = "markortleb.plugins.lsp" }, 
   { import = "markortleb.plugins.mason" },
   { import = "markortleb.plugins.mason-lspconfig" },
+   
+  -- Code Completion
+  { import = "markortleb.plugins.cmp" },
 
 
   { import = "markortleb.plugins.autopairs" },
   { import = "markortleb.plugins.treesitter" },
   { import = "markortleb.plugins.sqlui" },
-
-
-  { "tpope/vim-dadbod" },
-  { "kristijanhusak/vim-dadbod-ui" },
-  { "kristijanhusak/vim-dadbod-completion" }, 
+ 
 })
 
---
+++ nvim/lua/markortleb/plugins/lsp.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Python
+      lspconfig.pyright.setup({ capabilities = capabilities })
+
+      -- Lua
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+            workspace = { checkThirdParty = false }
+          }
+        }
+      })
+    end,
+  }
+}
+
