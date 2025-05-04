a17958458ccc3f44d65576d93b8c92b1f922a4d4
Mark Ortleb <markortleb@gmail.com>
2025-05-04 14:18:39 -0400

    setup codecompletion nvim to use ollama

--- nvim/lua/markortleb/plugins/tools/codecompanion.lua
+++ nvim/lua/markortleb/plugins/tools/codecompanion.lua
@@ -1,17 +1,32 @@
 return {
-  "olimorris/codecompanion.nvim",
-  opts = {
-    adapters = {
-      ollama = {
-        model = "codellama", -- Replace with your desired model
-        url = "http://localhost:11434/api/chat", -- Default Ollama chat endpoint
-      },
+    "olimorris/codecompanion.nvim",
+    opts = {
+        adapters = {
+            ollama = function()
+                return require("codecompanion.adapters").extend("ollama", {
+                    schema = {
+                        model = {
+                            default = "deepseek-coder:6.7b",
+                        },
+                        num_ctx = {
+                            default = 20000,
+                        },
+                    },
+                })
+            end,
+        },
+        strategies = {
+            chat = {
+                adapter = "ollama"
+            },
+            inline = {
+                adapter = "ollama"
+            }
+        }
     },
-    adapter = "ollama",
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
 }
 
