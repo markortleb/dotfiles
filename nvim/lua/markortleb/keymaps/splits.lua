4edcd3e5e8785dc0c6ab4cf7b2d1373787ee0ea7
Mark Ortleb <markortleb@gmail.com>
2025-06-20 22:20:56 -0400

    adadad

--- nvim/lua/markortleb/keymaps/splits.lua
+++ nvim/lua/markortleb/keymaps/splits.lua
@@ -1,44 +1,35 @@
--- Global resize factor and step list
-local resize_steps = { 10, 20, 30 }
-local step_index = 2 -- starts at 5
+-- Global resize factor and steps
+local resize_steps = {5, 10, 20}
+local step_index = 2 -- starts at 10
 vim.g.resize_factor = resize_steps[step_index]
 
--- Function to cycle resize factor
 local function cycle_resize_factor()
   step_index = step_index % #resize_steps + 1
   vim.g.resize_factor = resize_steps[step_index]
   print("Resize factor set to " .. vim.g.resize_factor)
 end
 
--- Keymap to cycle factor
-vim.keymap.set('n', '<leader>rf', cycle_resize_factor, { noremap = true, silent = false })
+-- Cycle resize factor
+vim.keymap.set('n', '<leader>ww', cycle_resize_factor, { noremap = true, silent = false })
 
 -- Resize mappings using current resize factor
-vim.keymap.set('n', '<leader>rh', function()
+vim.keymap.set('n', '<leader>wh', function()
   vim.cmd("vertical resize -" .. vim.g.resize_factor)
 end, { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>rl', function()
+vim.keymap.set('n', '<leader>wl', function()
   vim.cmd("vertical resize +" .. vim.g.resize_factor)
 end, { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>rj', function()
+vim.keymap.set('n', '<leader>wj', function()
   vim.cmd("resize -" .. vim.g.resize_factor)
 end, { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>rk', function()
+vim.keymap.set('n', '<leader>wk', function()
   vim.cmd("resize +" .. vim.g.resize_factor)
 end, { noremap = true, silent = true })
 
-
-
--- Create vertical split
-vim.keymap.set('n', '<leader>sv', ':vsplit<CR><C-w>l', { noremap = true, silent = true })
-
--- Create horizontal split
-vim.keymap.set('n', '<leader>sh', ':split<CR><C-w>j', { noremap = true, silent = true })
-
-
--- Set split close shortcut
-vim.keymap.set('n', '<leader>sc', ':close<CR>', { noremap = true, silent = true })
-
+-- Splits
+vim.keymap.set('n', '<leader>wv', ':vsplit<CR><C-w>l', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>w-', ':split<CR><C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wc', ':close<CR>', { noremap = true, silent = true })
--- nvim/lua/markortleb/keymaps/terminal.lua
+++ nvim/lua/markortleb/keymaps/terminal.lua
@@ -1,7 +1,5 @@
-
 -- Terminal Toggles
-
-vim.keymap.set('n', '<leader>t', ':ToggleTerm 1 dir=%:p:h<CR>', { noremap = true, silent = true }) -- convenience case, set to terminal 1
+vim.keymap.set('n', '<leader>t',  ':ToggleTerm 1 dir=%:p:h<CR>', { noremap = true, silent = true }) -- convenience case
 vim.keymap.set('n', '<leader>t1', ':ToggleTerm 1 dir=%:p:h<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>t2', ':ToggleTerm 2 dir=%:p:h<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>t3', ':ToggleTerm 3 dir=%:p:h<CR>', { noremap = true, silent = true })
@@ -9,6 +7,5 @@ vim.keymap.set('n', '<leader>t4', ':ToggleTerm 4 dir=%:p:h<CR>', { noremap = tru
 vim.keymap.set('n', '<leader>t5', ':ToggleTerm 5 dir=%:p:h<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>t6', ':ToggleTerm 6 dir=%:p:h<CR>', { noremap = true, silent = true })
 
-
 -- Exit terminal mode using Esc (from insert -> normal)
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
